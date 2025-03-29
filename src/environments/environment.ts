1d09faf49604919e6387958751e984dbb50e83a2
Mitanshu surana <55337570+mitanshusurana@users.noreply.github.com>
2025-03-29 16:21:27 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
-  production: false,
   r2: {
     endpoint: 'https://3145274f44bbf3178e1f2469ff4fdb07.r2.cloudflarestorage.com',
-    region: 'auto',
+    region: 'apac',
     credentials: {
       accessKeyId: '876f9fb20f5eefde33e5797efe255e5c',
       secretAccessKey: '84db7fe89e4de9de6b0c65b772c458efa64ec49482c48670695bfa90ec02ab12'
